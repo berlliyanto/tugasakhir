74dde83bd92867fd71fdbedbaff41500e5fd00af
berlliyanto <126399467+berlliyanto@users.noreply.github.com>
2023-03-05 11:59:13 +0700

    model number

--- models/energy.model.js
+++ models/energy.model.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const energy = mongoose.model(
     "energy",
     mongoose.Schema({
-        voltage: String,
-        current: String,
-        power: String,
-        energy: String,
-        frequency: String,
-        pf: String,
+        voltage: Number,
+        current: Number,
+        power: Number,
+        energy: Number,
+        frequency: Number,
+        pf: Number,
     },
     {
         timestamps: true,
